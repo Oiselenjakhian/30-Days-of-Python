c17924bd3edab1b4234aee98dab2d16f8f125d1b
Truston Ailende <trustonailende@gmail.com>
2022-11-27 13:26:19 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Accept the input of the first number
+first_number = float(input("Please enter the first number: "))
+
+# Accept the input of the second number
+second_number = float(input("Please enter the second number: "))
+
+# Find the sum
+print("The sum of", first_number, "and", second_number, "is", first_number + second_number)
+
+# Find the difference
+print("The difference of", first_number, "and", second_number, "is", first_number - second_number)
+
+# Find the product
+print("The product of", first_number, "and", second_number, "is", first_number * second_number)
+
+# Find the quotient
+print("The quotient of", first_number, "and", second_number, "is", first_number / second_number)
